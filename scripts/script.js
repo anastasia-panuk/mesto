509a2c18184663f3629854917a2fcc3f137887cf
Anastasia Panuk <AnaPanana@yandex.ru>
2022-06-07 10:47:10 +0300

    правки по второму ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,39 @@
-let popup = document.querySelector('.popup'); //получаем доступ к блоку '.popup'
-let editButton = document.querySelector('.user__edit-button'); //получаем доступ к кнопке редактирования профиля
-let popupCloseButton = document.querySelector('.popup__close-button'); //получаем доступ к кнопке закрытия попапа
+const popup = document.querySelector('.popup'); //получаем доступ к блоку '.popup'
+const editButton = document.querySelector('.user__edit-button'); //получаем доступ к кнопке редактирования профиля
+const popupCloseButton = document.querySelector('.popup__close-button'); //получаем доступ к кнопке закрытия попапа
 
-let popupForm = document.querySelector('.popup__form');//получаем доступ к форме попапа
+const popupForm = document.querySelector('.popup__form');//получаем доступ к форме попапа
 
-let userName = document.querySelector('.user__name'); //получаем доступ к элементу с именем пользователя
-let userProfile = document.querySelector('.user__profile'); //получаем доступ к элементу с профием пользователя
-let popupUserName = document.querySelector('#popup__user-name'); //получаем доступ к полю с именем пользователя
-let popupUserProfile =document.querySelector('#popup__user-profile'); //получаем доступ к полю профиля пользователя в попапе
+const userName = document.querySelector('.user__name'); //получаем доступ к элементу с именем пользователя
+const userProfile = document.querySelector('.user__profile'); //получаем доступ к элементу с профием пользователя
+const popupUserName = document.querySelector('#popup__user-name'); //получаем доступ к полю с именем пользователя
+const popupUserProfile =document.querySelector('#popup__user-profile'); //получаем доступ к полю профиля пользователя в попапе
 
 function togglePopup() {
-  popup.classList.toggle('popup_opened'); //открываем из закрываем попап, добавляя или убирая класс 'popup_opened'
-  popupUserName.value = userName.textContent; //передаем тектовым полям
-  popupUserProfile.value = userProfile.textContent;//значения из профиля
-}
+  popup.classList.toggle('popup_opened');
+} //функция вызова попапа
 
-function savePopup(evt) {
+function submitInputValueToPopupForm() {
+  popupUserName.value = userName.textContent;
+  popupUserProfile.value = userProfile.textContent;
+  togglePopup();
+}//функция передачи значений из профиля текстовым полям (открывает попап)
+
+
+function submitUserDataToServer(evt) {
   evt.preventDefault();
   userName.textContent = popupUserName.value;
   userProfile.textContent = popupUserProfile.value;
   togglePopup();
-} //объявляем функцию, которая сохраняет, отправляет данные пользователя и закрывает попап после отправки данных
+} //функция редактирования и отправки данных пользователя на сервер (закрывает попап)
 
-function closePopupFormByOverlay(evt) {
+function closePopupByOverlay(evt) {
   if(evt.target === evt.currentTarget) {
     togglePopup();
  }
- }//объявляем функцию, удаляющую класс 'popup_opened' из блока 'popup' по клику на оверлей и закрывает попап
+ }//функция закрытия попапа по клику на оверлей
 
-editButton.addEventListener('click', togglePopup);//"навешиваем" обрабочик событий по клику на кнопку редактирования профиля (открывает попап)
+editButton.addEventListener('click', submitInputValueToPopupForm);//"навешиваем" обрабочик событий по клику на кнопку редактирования профиля (открывает попап)
 popupCloseButton.addEventListener('click', togglePopup);//"навешиваем" обрабочик событий по клику на кнопку закрытия попапа (закрывает попап)
-popupForm.addEventListener('submit', savePopup);//"навешиваем" обрабочик событий на форму, сохраняющий и отправляющий данные пользователя
-popup.addEventListener('click', closePopupFormByOverlay);//"навешиваем" обрабочик событий по клику на попап, закрывающий попап по щелчку на оверлей (закрывает попап)
+popupForm.addEventListener('submit', submitUserDataToServer);//"навешиваем" обрабочик событий на форму, сохраняющий и отправляющий данные пользователя
+popup.addEventListener('click', closePopupByOverlay);//"навешиваем" обрабочик событий по клику на попап, закрывающий попап по щелчку на оверлей (закрывает попап)
