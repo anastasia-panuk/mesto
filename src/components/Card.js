43ed0822fad2706c534876c4bde31754823ceb5b
Anastasia Panuk <AnaPanana@yandex.ru>
2022-08-25 11:30:19 +0300

    feat: реализована новая функция постановки лайка

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,5 @@
 export default class Card {
-  constructor(data, { handleCardClick, cardTemplateSelector }) {
+  constructor(data, { handleCardClick, cardTemplateSelector, userId, addLike, deleteLike, deleteCard }) {
     this._cardTemplateSelector = cardTemplateSelector;
     this._card = document
       .querySelector(this._cardTemplateSelector)
@@ -9,28 +9,68 @@ export default class Card {
     this._cardTitle = this._card.querySelector('.card__name');
     this._cardLikeBtn = this._card.querySelector('.card__like-button');
     this._cardTrashBtn = this._card.querySelector('.card__trash-button');
+    this._cardLikesCounter = this._card.querySelector('.card__like-counter');
     this._cardName = data.name;
     this._cardLink = data.link;
+    this._cardLike = data.likes;
+    this._cardOwnerId = data.owner._id;
+    this._cardId = data._id;
     this._handleCardClick = handleCardClick;
+    this._userId = userId;
+    this._addLike = addLike;
+    this._deleteLike = deleteLike;
+    this._deleteCard = deleteCard;
   }
 
   createCard() {
     this._setEventListener();
+    this._isCardCreatedByUser();
 
     this._cardImage.src = this._cardLink;
     this._cardImage.alt = this._cardName;
     this._cardTitle.textContent = this._cardName;
+    this._cardLikesCounter.textContent = this._cardLike.length;
 
+    this._isCardLikedByUser()
     return this._card;
   }
 
   _handlerLikeButton() {
-    this._cardLikeBtn.classList.toggle('card__like-button_active');
+    if(!this._cardLikeBtn.classList.contains('card__like-button_active')) {
+      this._addLike(this._cardId)
+      .then((res) => {
+        this._cardLikeBtn.classList.add('card__like-button_active');
+        this._cardLikesCounter.textContent = res.likes.length;
+       })
+       .catch((err) => console.log(err))
+    } else{
+      this._deleteLike(this._cardId)
+      .then((res) => {
+        this._cardLikeBtn.classList.remove('card__like-button_active');
+        this._cardLikesCounter.textContent = res.likes.length;
+       })
+       .catch((err) => console.log(err))
+    }
+  }
+
+  _isCardLikedByUser() {
+    if(this._cardLike.some(el => el._id === this._userId)) {
+      this._cardLikeBtn.classList.add('card__like-button_active');
+    }
+  }
+
+  _isCardCreatedByUser() {
+    if(this._cardOwnerId !== this._userId) {
+      this._cardTrashBtn.remove()
+    }
   }
 
   _deleteCardByButton() {
-    this._card.remove();
-    this._card = null;
+    const data = {
+      card: this._card,
+      cardId: this._cardId
+    }
+    this._deleteCard(data);
   }
 
   _setEventListener() {
